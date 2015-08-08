efba76d70b350f3441dfaddfe026aae78c082e2f
token4 <thomas.j.kunjappu@gmail.com>
2015-08-08 15:44:42 -0400

    Added functions to Enemy and Player and got them rendering

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    // Initial location of enemy
+    this.x = 1;
+    this.y = 60;
+    //speed factor will be a number between 100 & 300
+    this.speed = Math.floor(Math.random()*(200)+100);
 }
 
 // Update the enemy's position, required method for game
@@ -14,6 +16,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed*dt;
+
 }
 
 // Draw the enemy on the screen, required method for game
@@ -25,12 +29,36 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function() {
+  this.sprite = 'images/char-horn-girl.png';
+  this.x = 200;
+  this.y = 400;
+}
+
+Player.prototype.update = function(dt) {
+
+}
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(key) {
+//need to fill in moves for various key presses
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var bug1 = new Enemy;
+var bug2 = new Enemy;
+var bug3 = new Enemy;
+bug2.y += 85;
+bug3.y += 170;
+var allEnemies = [];
+allEnemies.push(bug1, bug2, bug3);
+var player = new Player;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
