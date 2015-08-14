c8bc9af27e43cedc362647af2ce3b1080643456e
token4 <thomas.j.kunjappu@gmail.com>
2015-08-13 17:36:55 -0700

    Proper syntax fixes from code-review: semicolons, vars etc fixed

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var Enemy = function(startX, startY) {
     this.y = startY;
     //speed factor will be a number between 75 & 275
     this.speed = Math.floor(Math.random()*(200)+75);
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -16,103 +16,102 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    newx = this.x +=this.speed*dt
+    var newx = this.x +=this.speed*dt;
     if ( (newx) > 500) {
-      this.x = 0;
-      this.speed = Math.floor(Math.random()*(200)+75);
+        this.x = 0;
+        this.speed = Math.floor(Math.random()*(200)+75);
     }
     else {
-      this.x = newx;
-    };
-
-}
+        this.x = newx;
+    }
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
 var Player = function() {
-  this.sprite = 'images/char-horn-girl.png';
-  this.x = 200;
-  this.y = 400;
-}
+    this.sprite = 'images/char-horn-girl.png';
+    this.x = 200;
+    this.y = 400;
+};
 
 Player.prototype.update = function(dt) {
   //if there is a collision with one of the enemies, reset player
-  for (i=0; i < allEnemies.length; i++) {
-    if (Math.abs(this.x-allEnemies[i].x)<35 &&
-        Math.abs(this.y-allEnemies[i].y)<35) {
-      this.reset("loss");
+    for (var i = 0; i < allEnemies.length; i++) {
+        if (Math.abs(this.x-allEnemies[i].x)<35 &&
+            Math.abs(this.y-allEnemies[i].y)<35) {
+            this.reset("loss");
+        }
     }
-  }
-}
+};
 
 Player.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 Player.prototype.handleInput = function(key) {
 //need to fill in moves for various key presses
-  if (key == 'left') {
-    if (this.x - 100 >= 0) {
-      this.x -= 100;
+    if (key == 'left') {
+        if (this.x - 101 >= 0) {
+            this.x -= 101;
+          }
     }
-  }
-  else if (key == 'right') {
-    if (this.x + 100 < 500) {
-      this.x += 100;
+    else if (key == 'right') {
+        if (this.x + 101 < 500) {
+            this.x += 101;
+          }
     }
-  }
-  else if (key == 'up') {
-    if (this.y - 82 >= 71) {
-      this.y -= 82;
+    else if (key == 'up') {
+        if (this.y - 83 >= 71) {
+            this.y -= 83;
+          }
+        //reset when you reach the water
+        else {
+          this.reset("win");
+        }
     }
-    //reset when you reach the water
-    else {
-      this.reset("win");
+    else if (key == 'down') {
+        if (this.y + 83 <= 401) {
+            this.y += 83;
+          }
     }
-  }
-  else if (key == 'down') {
-    if (this.y + 82 <= 401) {
-      this.y += 82;
-    }
-  }
-
-}
+};
 
 Player.prototype.reset = function(status) {
-  this.x = 200;
-  this.y = 400;
-  //update and show number of wins
-  if (status == "win") {
-    winCount++;
-    document.getElementById("win").innerHTML = "Wins: " +winCount;
-  }
-  //update and show number of losses
-  else if (status == "loss") {
-    lossCount++;
-    document.getElementById("loss").innerHTML = "Losses: " +lossCount;
-  }
-  //not sure this else is even needed
-  else {
-    console.log("Something's weird about how the player got reset")
-  }
-}
+    //reset player to original location
+    this.x = 200;
+    this.y = 400;
+    //update and show number of wins
+    if (status == "win") {
+      winCount++;
+      document.getElementById("win").innerHTML = "Wins: " +winCount;
+    }
+    //update and show number of losses
+    else if (status == "loss") {
+      lossCount++;
+      document.getElementById("loss").innerHTML = "Losses: " +lossCount;
+    }
+    //not sure this else is even needed
+    else {
+      console.log("Something's weird about how the player got reset");
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var winCount = lossCount = 0;
+var winCount = var lossCount = 0;
 var allEnemies = [];
-for (i = 0; i < 3; i++) {
-  allEnemies.push(new Enemy(0, 60+(85*i)));
+for (var i = 0; i < 3; i++) {
+    allEnemies.push(new Enemy(0, 60+(85*i)));
 }
-var player = new Player;
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
